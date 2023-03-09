8deac52a249e7d54a67e9288be78d94a28c186d1
crtneko97 <simon_kern@hotmail.com>
2023-03-09 11:58:42 +0100

    school project changed to java17

---
+++ src/main/java/com/example/demoMedSimon/DemoMedSimonApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoMedSimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMedSimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMedSimonApplication.class, args);
+		
+	}
+
+}
